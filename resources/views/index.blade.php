90d95702e9e66e5217f443af7a0170690fb4c5a3
Rahul Tummala <rahul@Rahuls-MBP.attlocal.net>
2024-03-20 19:13:43 -0400

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<h1>List of tasks</h1>
+
+<div>
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                {{ $task->title }}
+            </a>
+        </div>
+    @empty
+        <div>There are no tasks</div>
+    @endforelse
+
+</div>
